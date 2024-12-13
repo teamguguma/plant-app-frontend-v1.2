167a637784590e85ecedc17824d914e3b8e16183
Bogyeong Kim <bogyeongkim614@gmail.com>
2024-12-13 15:13:14 +0900

    feat: 식물 촬영 후 안내 팝업

--- app/src/main/java/com/guguma/guguma_application/MainActivity.kt
+++ app/src/main/java/com/guguma/guguma_application/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.guguma.guguma_application
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 //import androidx.preference.PreferenceManager
 import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
